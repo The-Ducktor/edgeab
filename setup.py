d38be0399c0cfa3ada8baf5ef9da770cc76615cb
The Ducktor <130664593+The-Ducktor@users.noreply.github.com>
2024-07-19 08:41:44 +0100

    Clean Up (#6)
    * ✨ Remove unused imports and functions
    
    * ✨ refactor: Remove unnecessary imports and functions / Rename To main.py
    
    * 🔧 Updated dependencies in requirements.txt and refactored setup.py configuration.
    
    ---------
    
    Co-authored-by: The Ducktor <theducktor@duck.com>

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="epub2tts-edge",
+    name="edgeab",
     description="Tool to read an epub to audiobook using MS Edge TTS",
-    author="Christopher Aedo aedo.dev",
-    author_email="c@aedo.dev",
-    url="https://github.com/aedocw/epub2tts-edge",
+    author="The Ducktor",
+    author_email="ninjastealth99@proton.me",
+    url="https://github.com/TheDucktor/abedge",
     license="GPL 3.0",
-    version="1.1.9",
+    version="0.1",
     packages=find_packages(),
     install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'epub2tts-edge = epub2tts_edge:main'
+            'edgeab = :main'
         ]
     },
 )
