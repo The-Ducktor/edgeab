12d6fab10caf8001fe1bcdda2f3084545d36712d
The Ducktor <theducktor@duck.com>
2024-09-08 00:59:17 +0800

    🔖 feat(phonics): Load phonics data from a configurable CSV file
    - Load phonics data from a configurable CSV file path instead of a hardcoded one
    - Add exception handling to the `phontify` function to gracefully handle errors
    - Update the chapter number display in the main script to show the current chapter and the total number of chapters

--- epub2tts_edge/phonics.py
+++ epub2tts_edge/phonics.py
@@ -2,7 +2,7 @@
 
 import re
 import csv
-
+csv_file = '/Users/jacks/Documents/Git/epub2tts-edge/epub2tts_edge/phonics.csv'
 # Function to load phonetic mappings from a CSV file
 def load_phonics_from_csv(filename):
     """Load phonetic mappings from a CSV file."""
@@ -15,17 +15,20 @@ def load_phonics_from_csv(filename):
 
 # Function to replace character names with their phonetic counterparts
 def phontify(text):
-    """Replace character names in the text with their phonic counterparts."""
-    phonics = load_phonics_from_csv('phonics.csv')
-    for name, phonic in phonics.items():
-        # Use regular expression to replace names (case insensitive)
-        text = re.sub(r'\b' + re.escape(name) + r'\b', phonic, text, flags=re.IGNORECASE)
-    return text
+    try:
+        """Replace character names in the text with their phonic counterparts."""
+        phonics = load_phonics_from_csv(csv_file)
+        for name, phonic in phonics.items():
+            # Use regular expression to replace names (case insensitive)
+            text = re.sub(r'\b' + re.escape(name) + r'\b', phonic, text, flags=re.IGNORECASE)
+        return text
+    except Exception:
+        return text
 
 # Example usage
 if __name__ == "__main__":
     input_text = """
-    Hinata Sakaguchi and Milim Nava arrived at Rimuru's castle. "Karin, could you bring the documents?" asked Rimuru. Gabil was also present.
+    Hinata Sakaguchi and Milim Nava arrived aat Rimuru's castle. "Karin, could you bring the documents?" asked Rimuru. Gabil was also present.
     """
     
     output_text = phontify(input_text)
