51781a7cb0a48870fbd94999b8a93079179ab245
The Ducktor <130664593+The-Ducktor@users.noreply.github.com>
2024-09-08 09:37:38 +0800

    Phonetic Names (#9)
    * ✨ add : Add phontify method to replace character names with phonic counterparts.
    
    * ✨ Refactor: Update phontify function in phonics.py
    
    Updated the phontify function in phonics.py to replace character names
    from the text with their phonic counterparts.
    
    * ✨ Add csv support for loading phonics mappings
    
    Add support for loading phonics mappings from a CSV file to replace character
    names in the text with their phonic counterparts.
    
    * ✨ Add function to load phonetic mappings from CSV file
    
    * 🔖 feat(phonics): Load phonics data from a configurable CSV file
    
    - Load phonics data from a configurable CSV file path instead of a hardcoded one
    - Add exception handling to the `phontify` function to gracefully handle errors
    - Update the chapter number display in the main script to show the current chapter and the total number of chapters
    
    ---------
    
    Co-authored-by: The Ducktor <theducktor@duck.com>

---
+++ epub2tts_edge/phonics.py
@@ -0,0 +1,35 @@
+# sentence.py
+
+import re
+import csv
+csv_file = '/Users/jacks/Documents/Git/epub2tts-edge/epub2tts_edge/phonics.csv'
+# Function to load phonetic mappings from a CSV file
+def load_phonics_from_csv(filename):
+    """Load phonetic mappings from a CSV file."""
+    phonics = {}
+    with open(filename, mode='r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            phonics[row['name']] = row['phonetic']
+    return phonics
+
+# Function to replace character names with their phonetic counterparts
+def phontify(text):
+    try:
+        """Replace character names in the text with their phonic counterparts."""
+        phonics = load_phonics_from_csv(csv_file)
+        for name, phonic in phonics.items():
+            # Use regular expression to replace names (case insensitive)
+            text = re.sub(r'\b' + re.escape(name) + r'\b', phonic, text, flags=re.IGNORECASE)
+        return text
+    except Exception:
+        return text
+
+# Example usage
+if __name__ == "__main__":
+    input_text = """
+    Hinata Sakaguchi and Milim Nava arrived aat Rimuru's castle. "Karin, could you bring the documents?" asked Rimuru. Gabil was also present.
+    """
+    
+    output_text = phontify(input_text)
+    print(output_text)
